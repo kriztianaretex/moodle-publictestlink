a18a952595ea2c1ece5ec10cf9876e6b745ff48b
azelynn-aretex <azelynn-aretex@gmail.com>
2026-02-05 12:07:06 +1100

    Add working hello world

---
+++ lib.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @package local_publictestlink
+ * @author azi-team
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+function local_publictestlink_before_footer() {
+    \core\notification::add('Hello World!', \core\output\notification::NOTIFY_SUCCESS);
+}
