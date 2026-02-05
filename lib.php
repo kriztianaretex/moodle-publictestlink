19907fb8fa788174e4504830c4fdfe0a834bd7b1
azelynn-aretex <azelynn-aretex@gmail.com>
2026-02-05 14:27:41 +1100

    Add success message for pulling commit.

--- lib.php
+++ lib.php
@@ -8,4 +8,5 @@
 
 function local_publictestlink_before_footer() {
     \core\notification::add('Hello World!', \core\output\notification::NOTIFY_SUCCESS);
+    \core\notification::add('You have successfully pulled my commit.', \core\output\notification::NOTIFY_INFO);
 }
