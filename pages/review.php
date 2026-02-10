069e940897b3954c2e70f7d519d4256ab72fd56b
azelynn-aretex <azelynn-aretex@gmail.com>
2026-02-11 10:22:37 +1100

    Add review page

---
+++ pages/review.php
@@ -0,0 +1,60 @@
+<?php
+require_once('../../../config.php');
+require_once($CFG->libdir . '/questionlib.php');
+require_once('../classes/attempt.php');
+require_once('../classes/session.php');
+
+use core\url as moodle_url;
+use core\notification;
+
+// TODO check if attempt is completed
+
+$cmid = required_param('cmid', PARAM_INT);
+$attemptid = required_param('attemptid', PARAM_INT);
+
+$session = publictestlink_session::check_session();
+if ($session == null) {
+    redirect(
+        new moodle_url($PLUGIN_URL . '/landing.php', ['cmid' => $cmid]),
+        'You are not logged in.', null, notification::ERROR
+    );
+    return;
+}
+
+$cm = get_coursemodule_from_id('quiz', $cmid, 0, false, MUST_EXIST);
+$quiz = $DB->get_record('quiz', ['id' => $cm->instance], '*', MUST_EXIST);
+$context = context_module::instance($cm->id);
+
+$attempt = publictestlink_attempt::from_id($attemptid);
+
+if ($attempt->get_shadow_user()->get_id() !== $session->get_user()->get_id()) {
+    redirect(
+        new moodle_url($PLUGIN_URL . '/landing.php', ['cmid' => $cmid])
+    );
+    return;
+}
+
+$quba = $attempt->get_quba();
+$quba->set_preferred_behaviour($quiz->preferredbehaviour);
+
+$PAGE->set_url($PLUGIN_URL . '/local/publictestlink/review.php', ['attemptid' => $attemptid]);
+$PAGE->set_pagelayout('standard');
+$PAGE->set_title(get_string('review'));
+$PAGE->set_heading(get_string('review'));
+
+$displayoptions = new question_display_options();
+$displayoptions->readonly = true;
+$displayoptions->marks = question_display_options::MARK_AND_MAX;
+$displayoptions->correctness = question_display_options::VISIBLE;
+$displayoptions->feedback = question_display_options::VISIBLE;
+$displayoptions->rightanswer = question_display_options::VISIBLE;
+$displayoptions->history = question_display_options::VISIBLE;
+
+
+echo $OUTPUT->header();
+
+foreach ($quba->get_slots() as $slot) {
+    echo $quba->render_question($slot, $displayoptions);
+}
+
+echo $OUTPUT->footer();
